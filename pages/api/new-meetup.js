b43c20f571517f6a3b24d002d91d091fecf56fd8
Dhrumil Pandya <dvpandya2138433@gmail.com>
2023-09-01 17:59:46 +0530

    feat: add new meetup feature

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+    const client = await MongoClient.connect(
+      "mongodb+srv://dvpandya2138433:Uy97b9fTsqCqbqA7@cluster0.lxv8r8t.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({message: "Data Inserted!"});
+  }
+}
+
+export default handler;
